4638516f70c8dd6766444257de5713d1ab7fff9b
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2022-06-14 10:46:05 +0100

    Add more Metrics that can be collected by MetricCollector
    These are ALL_VEHICLE_IDS, WALL_TIME, and MESSAGES_EXCHANGED.

--- intersection_control/core/performance_indication.py
+++ intersection_control/core/performance_indication.py
@@ -1,6 +1,7 @@
-from typing import List, Any, Dict, Callable, Tuple, Optional
+from typing import List, Any, Dict, Callable, Tuple, Optional, Set, Type
+from timeit import default_timer as timer
 import numpy as np
-from intersection_control.core import Environment
+from intersection_control.core import Environment, MessagingUnit
 
 
 class Metric:
@@ -12,10 +13,13 @@ class Metric:
     ALL_POSITIONS = 5
     NUM_VEHICLES = 6
     NUM_VEHICLES_BY_TRAJECTORY = 7
+    ALL_VEHICLE_IDS = 8
+    WALL_TIME = 9
+    MESSAGES_EXCHANGED = 10
 
 
 class MetricCollector:
-    def __init__(self, environment: Environment, *metrics):
+    def __init__(self, environment: Environment, *metrics, messaging_unit_class: Optional[Type[MessagingUnit]] = None):
         self.environment = environment
         self.results: Dict[int, List[Any]] = {metric: [] for metric in metrics}
         self.poll_function: Dict[int, Callable[[], Any]] = {
@@ -27,7 +31,19 @@ class MetricCollector:
             Metric.ALL_POSITIONS: self._poll_all_positions,
             Metric.NUM_VEHICLES: self._poll_num_vehicles,
             Metric.NUM_VEHICLES_BY_TRAJECTORY: self._poll_num_vehicles_by_trajectory,
+            Metric.ALL_VEHICLE_IDS: self._poll_all_vehicle_ids,
+            Metric.WALL_TIME: self._poll_wall_time,
+            Metric.MESSAGES_EXCHANGED: self._poll_messages_exchanged,
         }
+        if Metric.WALL_TIME in metrics:
+            self.last_wall_time: Optional[float] = None
+
+        if Metric.MESSAGES_EXCHANGED in metrics:
+            assert messaging_unit_class is not None
+            self.last_num_send_calls = 0
+            self.num_send_calls = 0
+            # A little bit of python magic to be able to count the number of calls to send()
+            messaging_unit_class.send = self._count_send_calls(messaging_unit_class.send)
 
     def poll(self):
         for metric, result in self.results.items():
@@ -68,3 +84,24 @@ class MetricCollector:
         for v in self.environment.vehicles.get_ids():
             result[self.environment.vehicles.get_trajectory(v)] += 1
         return result
+
+    def _poll_all_vehicle_ids(self) -> Set[str]:
+        return set(self.environment.vehicles.get_ids())
+
+    def _poll_wall_time(self) -> float:
+        time = timer()
+        result = (time - self.last_wall_time) if self.last_wall_time is not None else 0
+        self.last_wall_time = time
+        return result
+
+    def _poll_messages_exchanged(self) -> int:
+        result = self.num_send_calls - self.last_num_send_calls
+        self.last_num_send_calls = self.num_send_calls
+        return result
+
+    def _count_send_calls(self, send):
+        def wrapper(instance, address, message):
+            self.num_send_calls += 1
+            send(instance, address, message)
+
+        return wrapper
